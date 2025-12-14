18caff412b0b3f044c1c9e56d84f15f00a995676
Thato Mapheto <thatomapheto6@gmail.com>
2025-12-14 19:55:40 +0200

    fix error

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,32 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-
+    # Use serializers.CharField() without parameters initially
+    password = serializers.CharField()
+    password2 = serializers.CharField()
+    
     class Meta:
         model = User
         fields = ('email', 'username', 'password', 'password2', 'bio')
-
+    
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        # Add validation manually since we removed validators parameter
+        if attrs.get('password') != attrs.get('password2'):
             raise serializers.ValidationError({"password": "Password fields didn't match."})
+        
+        # Manually validate password
+        try:
+            validate_password(attrs.get('password'))
+        except Exception as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+        
         return attrs
-
+    
     def create(self, validated_data):
         validated_data.pop('password2')
         user = get_user_model().objects.create_user(**validated_data)
@@ -25,11 +34,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         return user
 
 class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField()
+    password = serializers.CharField()
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'username', 'bio', 'profile_picture')
-        read_only_fields = ('email',)
